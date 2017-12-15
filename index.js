e52dc7b69c6063635457f4bfcfdd8eb21e82f399
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-15 23:11:16 +0530

    Made own app by changing index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
+// Create a component
+const App = () => (
+    <Text>Some Text</Text>
+  );
+
+// Render it to the device
 AppRegistry.registerComponent('albums', () => App);
