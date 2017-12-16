77156d46c186e3b39dbed70091570ca990d32183
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-17 01:39:57 +0530

    AlbumList.js: Use of componentWillMount

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+  }
+
   render() {
     return (
       <View>
