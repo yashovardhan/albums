b7f48bc1a2f48d39860a248833ded0ea34461933
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-17 01:14:30 +0530

    used props to make header test dynamic

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make component
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
