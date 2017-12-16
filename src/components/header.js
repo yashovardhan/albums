5c944035636f9afb540e2e15df03235f0092ec33
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-17 01:04:08 +0530

    Made a header using various style props

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,34 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make component
-const Header = () => (
-  <Text>Albums</Text>
-);
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
 
 // Make the component available to other parts of the app
 export default Header;
