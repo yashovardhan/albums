3b9216e73adc17e61160c416a0e9c9ad11eb1e8c
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-17 00:42:08 +0530

    Made a header component in an external file

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make component
+const Header = () => (
+  <Text>Albums</Text>
+);
+
+// Make the component available to other parts of the app
+export default Header;
