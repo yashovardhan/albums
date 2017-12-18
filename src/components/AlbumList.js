f030f5c4a7f616d41d716ed95e250ee8196be2ff
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-19 00:13:53 +0530

    display album names
    * use axios to fetch json data from the URL
    * set initial state as empty
    * using setState property feed response data to state
    * display title names using the fetched data in the state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+      axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <Text key={album.title}>{album.title}</Text>);
   }
 
   render() {
+    console.log(this.state);
+
     return (
       <View>
-        <Text>Album List</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
