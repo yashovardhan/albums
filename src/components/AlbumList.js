77b7b1aa5f89f410241a003c570d60923fd0060c
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-21 20:26:32 +0530

    Add AlbumDetail, Card & CardSection

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -12,7 +13,7 @@ class AlbumList extends Component {
 
   renderAlbums() {
     return this.state.albums.map(album =>
-      <Text key={album.title}>{album.title}</Text>);
+      <AlbumDetail key={album.title} album={album} />);
   }
 
   render() {
